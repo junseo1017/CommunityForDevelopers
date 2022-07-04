a6496f4458bd673b1eaa99322db4e6c938fae440
junseolee <dlwnstjzld@gmail.com>
2022-07-04 22:10:20 +0900

    initialize

---
+++ templates/front-end/pages/_document.js
@@ -0,0 +1,21 @@
+import {Html, Head, Main, NextScript} from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' />
+        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='annonymous' />
+        <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap' rel='stylesheet' />
+        <title>CFD</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
