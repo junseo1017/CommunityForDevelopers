2d11c95460f20bed3ff35170f606049c3f87bd91
junseolee <dlwnstjzld@gmail.com>
2022-07-04 23:16:57 +0900

    next.js 구조  세팅

--- templates/front-end/pages/_app.js
+++ templates/front-end/pages/_app.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import Head from 'next/head';
+import 'antd/dist/antd.css';
+import wrapper from '../store/index';
 const MyApp = ({Component, pageProps}) => {
   return (
     <>
@@ -12,4 +16,4 @@ const MyApp = ({Component, pageProps}) => {
   );
 };
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
