db632e97fd44c7f8a7951d09a7da153c98d0f983
junseolee <dlwnstjzld@gmail.com>
2022-07-04 23:08:13 +0900

    bug fix

--- templates/front-end/pages/_app.js
+++ templates/front-end/pages/_app.js
@@ -1,12 +1,6 @@
 const MyApp = ({Component, pageProps}) => {
   return (
     <>
-      <Head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' />
-        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
-        <title>CFD</title>
-      </Head>
       <Component {...pageProps} />
     </>
   );
