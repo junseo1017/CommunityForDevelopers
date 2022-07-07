73a26e8336e9eeaf4fdc0c690cf8cd6d7eafaf18
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-07 11:03:33 +0900

    QnA 게시판 질문 상세 페이지 레이아웃 수정 2

--- templates/front-end/components/Questions/QuestionDetail/QuestionDetail.js
+++ templates/front-end/components/Questions/QuestionDetail/QuestionDetail.js
@@ -1,103 +1,91 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
 import TopButton from "../TopButton";
-import { Row, Col, Button, Badge, Divider } from "antd";
-import { QuestionOutlined, StarOutlined, CommentOutlined } from "@ant-design/icons";
+import Comments from "./Comments";
+import { Button, Badge, Divider, Collapse, Panel } from "antd";
+import { QuestionOutlined, LikeOutlined } from "@ant-design/icons";
+import { FlexBox, ColFlexBox, CommentsContainer } from "../styles/QuestionStyle";
 
 const dummy_answers = [
   {
     title: "Answer 1",
     content:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis impedit, illum tenetur placeat excepturi tempore qui corrupti eaque deleniti quae libero similique. Harum numquam assumenda eaque? Culpa quibusdam veritatis a. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo molestiae quod quos consequuntur? Odio soluta, fugiat sint facilis aperiam, illum nisi reprehenderit, nemo aliquam quibusdam eum ipsa accusamus voluptates accusantium. Sunt numquam repudiandae esse fugit, quisquam dolorum voluptate libero eaque optio officia est, nemo molestias aliquid! Tempore deserunt ipsum maiores laborum magni odit incidunt deleniti! Officia repudiandae facilis corrupti itaque! Consequatur placeat unde distinctio officia ducimus dignissimos odit exercitationem. Saepe eius aspernatur consequuntur nostrum. Maxime corporis ipsam voluptates debitis aspernatur, praesentium architecto expedita sed itaque unde corrupti dignissimos voluptatibus nostrum! Laboriosam amet fugit sint quos quasi. Voluptatum quos optio blanditiis sequi, nostrum culpa voluptatem consectetur. Doloribus illum fuga provident similique dicta autem in.",
-    recommendations: 7,
+    like: 7,
   },
   {
     title: "Answer 2",
     content:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis impedit, illum tenetur placeat excepturi tempore qui corrupti eaque deleniti quae libero similique. Harum numquam assumenda eaque? Culpa quibusdam veritatis a. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo molestiae quod quos consequuntur? Odio soluta, fugiat sint facilis aperiam, illum nisi reprehenderit, nemo aliquam quibusdam eum ipsa accusamus voluptates accusantium. Sunt numquam repudiandae esse fugit, quisquam dolorum voluptate libero eaque optio officia est, nemo molestias aliquid! Tempore deserunt ipsum maiores laborum magni odit incidunt deleniti! Officia repudiandae facilis corrupti itaque! Consequatur placeat unde distinctio officia ducimus dignissimos odit exercitationem. Saepe eius aspernatur consequuntur nostrum. Maxime corporis ipsam voluptates debitis aspernatur, praesentium architecto expedita sed itaque unde corrupti dignissimos voluptatibus nostrum! Laboriosam amet fugit sint quos quasi. Voluptatum quos optio blanditiis sequi, nostrum culpa voluptatem consectetur. Doloribus illum fuga provident similique dicta autem in.",
-    recommendations: 11,
+    like: 11,
   },
   {
     title: "Answer 3",
     content:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis impedit, illum tenetur placeat excepturi tempore qui corrupti eaque deleniti quae libero similique. Harum numquam assumenda eaque? Culpa quibusdam veritatis a. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo molestiae quod quos consequuntur? Odio soluta, fugiat sint facilis aperiam, illum nisi reprehenderit, nemo aliquam quibusdam eum ipsa accusamus voluptates accusantium. Sunt numquam repudiandae esse fugit, quisquam dolorum voluptate libero eaque optio officia est, nemo molestias aliquid! Tempore deserunt ipsum maiores laborum magni odit incidunt deleniti! Officia repudiandae facilis corrupti itaque! Consequatur placeat unde distinctio officia ducimus dignissimos odit exercitationem. Saepe eius aspernatur consequuntur nostrum. Maxime corporis ipsam voluptates debitis aspernatur, praesentium architecto expedita sed itaque unde corrupti dignissimos voluptatibus nostrum! Laboriosam amet fugit sint quos quasi. Voluptatum quos optio blanditiis sequi, nostrum culpa voluptatem consectetur. Doloribus illum fuga provident similique dicta autem in.",
-    recommendations: 0,
+    like: 0,
   },
 ];
 
 const QuestionDetail = ({ questId }) => {
+  const onChange = (key) => {
+    console.log(key);
+  };
   return (
     <>
-      <Row justify="center" align="middle">
-        <Col>
-          <h1>Question Title({questId})</h1>
-        </Col>
-      </Row>
-      <Row justify="end">
-        <Col>
-          <a href="/">
-            <Button size="large" type="primary">
-              질문하기
-            </Button>
-          </a>
-        </Col>
-      </Row>
-      <Divider plain />
-      <Row justify="center" align="middle">
-        <Col span={16}>
+      <div css={ColFlexBox}>
+        <h1>Question Title({questId})</h1>
+        <a href="/questions/new">
+          <Button size="large" type="primary">
+            다른 질문하기
+          </Button>
+        </a>
+        <Divider plain />
+      </div>
+      <div css={ColFlexBox}>
+        <div css={FlexBox}>
           <QuestionOutlined style={{ fontSize: "2em" }} />
-          <h2>"Question Description"</h2>
+          <h2>Question Description</h2>
+        </div>
+        <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis impedit, illum tenetur
           placeat excepturi tempore qui corrupti eaque deleniti quae libero similique. Harum numquam
           assumenda eaque? Culpa quibusdam veritatis a. Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Quo molestiae quod quos consequuntur? Odio soluta, fugiat sint facilis
-          aperiam, illum nisi reprehenderit, nemo aliquam quibusdam eum ipsa accusamus voluptates
-          accusantium. Sunt numquam repudiandae esse fugit, quisquam dolorum voluptate libero eaque
-          optio officia est, nemo molestias aliquid! Tempore deserunt ipsum maiores laborum magni
-          odit incidunt deleniti! Officia repudiandae facilis corrupti itaque! Consequatur placeat
-          unde distinctio officia ducimus dignissimos odit exercitationem. Saepe eius aspernatur
-          consequuntur nostrum. Maxime corporis ipsam voluptates debitis aspernatur, praesentium
-          architecto expedita sed itaque unde corrupti dignissimos voluptatibus nostrum! Laboriosam
-          amet fugit sint quos quasi. Voluptatum quos optio blanditiis sequi, nostrum culpa
-          voluptatem consectetur. Doloribus illum fuga provident similique dicta autem in.
-        </Col>
-      </Row>
-      <Divider plain />
-      <Row justify="center" align="middle">
-        <Col span={16}>
-          <Badge count={dummy_answers[0].recommendations}>
-            <StarOutlined style={{ fontSize: "2em" }} />
-          </Badge>
-          <h2>{dummy_answers[0].title}</h2>
-          <p>{dummy_answers[0].content}</p>
-        </Col>
-      </Row>
-      <Divider plain />
-      <Row justify="center" align="middle">
-        <Col span={16}>
-          <Badge count={dummy_answers[1].recommendations}>
-            <StarOutlined style={{ fontSize: "2em" }} />
-          </Badge>
-          <h2>{dummy_answers[1].title}</h2>
-          <p>{dummy_answers[1].content}</p>
-        </Col>
-      </Row>
-      <Divider plain />
-      <Row justify="center" align="middle">
-        <Col span={16}></Col>
-      </Row>
-      <Divider plain />
-      <Row justify="center" align="middle">
-        <Col span={16}>
-          {/* Comments Component */}
-          <CommentOutlined style={{ fontSize: "2em" }} />
-          <h2>"Comment 1"</h2>
-          Rerum placeat esse nemo odio cum repellat. Nesciunt eius officia numquam quidem a sit
-          ipsum explicabo doloribus saepe sint accusamus enim similique id, voluptas pariatur soluta
-          eum doloremque laudantium dicta esse quae? Ratione impedit minima tempore nam. Nisi
-          consequuntur commodi quae alias delectus ipsum sit molestias quaerat ea molestiae. Facilis
-          vel libero ab blanditiis nam natus quo recusandae soluta veritatis laboriosam!
-        </Col>
-      </Row>
+          adipisicing elit. Quo molestiae quod quos consequuntur?
+        </p>
+        <a href="/answer">
+          <Button size="large" type="primary">
+            답변하기
+          </Button>
+        </a>
+        <Divider plain />
+      </div>
+      {dummy_answers.map((answer) => {
+        return (
+          <div css={ColFlexBox}>
+            <div css={FlexBox}>
+              <QuestionOutlined style={{ fontSize: "2em" }} />
+              <h2>{answer.title}</h2>
+            </div>
+            <p>{answer.content}</p>
+            <div css={FlexBox}>
+              <Badge count={answer.like}>
+                <LikeOutlined style={{ fontSize: "2em" }} />
+              </Badge>
+              <Button type="text">댓글 보기</Button>
+              {/* <Collapse defaultActiveKey={["1"]} onChange={onChange}>
+                <Panel header="This is panel header 1" key="1">
+                  {<Comments />}
+                </Panel>
+              </Collapse> */}
+            </div>
+            <Divider plain />
+          </div>
+        );
+      })}
+      <div css={CommentsContainer}>
+        <Comments />
+      </div>
       <Divider plain />
       <TopButton />
     </>
