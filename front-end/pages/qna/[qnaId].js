6cdbfd538a257b034888f90c5c0f5afd85402c8c
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-12 02:11:09 +0900

    서버에서 데이터 가져오기

--- front-end/pages/qna/[qnaId].js
+++ front-end/pages/qna/[qnaId].js
@@ -1,6 +1,15 @@
 import AppLayout from "../../components/AppLayout";
-import QuestionDetail from "../../components/Questions/QuestionDetail/QuestionDetail";
+// import QuestionDetail from "../../components/Questions/QuestionDetail/QuestionDetail";
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import axios from "axios";
+
+const QuestionDetail = dynamic(
+  () => {
+    return import("../../components/Questions/QuestionDetail/QuestionDetail");
+  },
+  { ssr: false },
+);
 
 const QuestionDetailPage = ({ qna }) => {
   return (
@@ -12,20 +21,17 @@ const QuestionDetailPage = ({ qna }) => {
 
 export default QuestionDetailPage;
 
-export async function getServerSideProps({ params }) {
-  console.log("context", params);
+export async function getServerSideProps({ query }) {
+  console.log("context", query);
+  const qnaId = query.qnaId;
 
-  try {
-    const response = await axios.get(`/api/qnas/${qnaId}`);
-    console.log("response: ", response.data);
-    const qna = response.data;
+  const response = await axios.get(`/api/qnas/${qnaId}`);
+  console.log("response: ", response.data);
+  const qna = response.data;
 
-    return {
-      props: {
-        qna,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-  }
+  return {
+    props: {
+      qna,
+    },
+  };
 }
---
+++ front-end/pages/qna/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Questions from "../../components/Questions/QuestionList/Questions";
+import AppLayout from "../../components/AppLayout";
+import Head from "next/head";
+import axios from "axios";
+// import { useDispatch, useSelector } from "react-redux";
+// import { qnaActions, getQnaData } from "../../reducers/qna";
+
+const questions = ({ qnas }) => {
+  // const dispatch = useDispatch();
+  // const qnas = useSelector((state) => state.qnas);
+
+  // useEffect(() => {
+  //   dispatch(getQnaData());
+  // }, [dispatch]);
+
+  // console.log(qnas);
+
+  const filterdQnas = qnas.filter((qna) => !qna.isAnswer);
+
+  return (
+    <AppLayout>
+      <Head>
+        <title>모든 질문</title>
+      </Head>
+      <Questions questions={filterdQnas} />
+    </AppLayout>
+  );
+};
+
+export default questions;
+
+export async function getServerSideProps(context) {
+  try {
+    console.log(context);
+    const response = await axios.get("/api/qnas");
+    console.log("response: ", response.data);
+    const qnas = response.data;
+
+    return {
+      props: {
+        qnas,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
