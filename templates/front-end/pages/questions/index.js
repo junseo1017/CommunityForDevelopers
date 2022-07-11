879c0010fbbb4ccca0dd56d55848f0962093f9e0
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-11 10:53:15 +0900

    axios 데이터 가져오기

--- templates/front-end/pages/questions/index.js
+++ templates/front-end/pages/questions/index.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Questions from "../../components/Questions/QuestionList/Questions";
 import AppLayout from "../../components/AppLayout";
 import Head from "next/head";
 import { dummy_qna } from "../../components/Questions/dummy";
+import axios from "axios";
 
 const questions = () => {
+  // 더미데이터 활용
   const questions = dummy_qna.filter((qna) => !qna.isAnswer);
+
+  const [qnas, setQnas] = useState([]);
+
+  useEffect(() => {
+    const getQnaData = async () => {
+      const response = await axios.get("/api/qna");
+      console.log(response);
+
+      setQnas(response);
+    };
+    getQnaData();
+  }, []);
 
   return (
     <AppLayout>
