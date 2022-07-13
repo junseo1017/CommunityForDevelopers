f01787be1aae04bc3d032524e8b0c394471de6af
junseolee <dlwnstjzld@gmail.com>
2022-07-13 17:28:58 +0900

    포트폴리오 가져오기 api 작업

--- front-end/actions/portfolio.js
+++ front-end/actions/portfolio.js
@@ -155,7 +155,6 @@ export const loadMyPortfolios = createAsyncThunk(
     try {
       const response = await axios.get(`api/portfolios/user/${data}`);
       console.log(response.data);
-
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
