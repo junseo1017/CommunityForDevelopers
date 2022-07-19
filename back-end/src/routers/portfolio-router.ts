35e6b8053cf79c187731394b66b30b9215ffaf35
TJ <xowns1016@gmail.com>
2022-07-19 16:17:25 +0900

    seach 기능 완성

--- back-end/src/routers/portfolio-router.ts
+++ back-end/src/routers/portfolio-router.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from "express";
-import { portfolioService } from "../services/portfolio-service";
+import { portfolioService, userService } from "../services";
 import { ExtendReq, loginRequired } from "../middlewares/login-required";
 
 const portfolioRouter = Router();
@@ -30,26 +30,26 @@ portfolioRouter.get(
   }
 );
 
-portfolioRouter.get(
-  "/search/list",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const options = req.query.option as string[];
-      const value = req.query.value as string;
-      const orderBy = req.query.orderBy as string;
-      const skills = req.query.skill as string[];
-      const searchInfo = { options, value, orderBy, skills };
-      const page = parseInt(req.query.page as string);
-      const Portfolios = await portfolioService.getPortfoliosBySearch(
-        searchInfo,
-        page
-      );
-      res.status(200).json(Portfolios);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
+// portfolioRouter.get(
+//   "/search/list",
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const options = req.query.option as string[];
+//       const value = req.query.value as string;
+//       const orderBy = req.query.orderBy as string;
+//       const skills = req.query.skill as string[];
+//       const searchInfo = { options, value, orderBy, skills };
+//       const page = parseInt(req.query.page as string);
+//       const Portfolios = await portfolioService.getPortfoliosBySearch(
+//         searchInfo,
+//         page
+//       );
+//       res.status(200).json(Portfolios);
+//     } catch (error) {
+//       next(error);
+//     }
+//   }
+// );
 
 portfolioRouter.get(
   "/user/:userId",
@@ -82,10 +82,15 @@ portfolioRouter.post(
   loginRequired,
   async (req: ExtendReq, res: Response, next: NextFunction) => {
     try {
-      const author = req.currentUserId || "";
+      const authorId = req.currentUserId || "";
+      const userInfo = await userService.getUserInfo(authorId);
+      const author = userInfo.nickname;
+      const authorImg = userInfo.imgUrl;
       const { title, description, skills, content, contentText } = req.body;
       const newPortfolio = await portfolioService.addPortfolio({
+        authorId,
         author,
+        authorImg,
         title,
         description,
         skills,
@@ -127,7 +132,7 @@ portfolioRouter.put(
   async (req: ExtendReq, res: Response, next: NextFunction) => {
     try {
       const portId = req.params.portId;
-      const author = req.currentUserId || "";
+      const userId = req.currentUserId || "";
       const { title, description, skills, content, contentText } = req.body;
       const toUpdate = {
         ...(title && { title }),
@@ -138,7 +143,7 @@ portfolioRouter.put(
       };
       const updatedPortfolioInfo = await portfolioService.setPortfolio(
         portId,
-        author,
+        userId,
         toUpdate
       );
       res.status(200).json(updatedPortfolioInfo);
@@ -154,10 +159,10 @@ portfolioRouter.delete(
   async (req: ExtendReq, res: Response, next: NextFunction) => {
     try {
       const portId = req.params.portId;
-      const author = req.currentUserId || "";
+      const userId = req.currentUserId || "";
       const deletedPortfolioInfo = await portfolioService.deletePortfolio(
         portId,
-        author
+        userId
       );
       res.status(200).json(deletedPortfolioInfo);
     } catch (error) {
