518562f003d4df64db50a136087973151ab21da1
junseolee <dlwnstjzld@gmail.com>
2022-07-22 00:31:24 +0900

    스크랩 에러 처리

--- front-end/components/userProfile/ProfileScrap.jsx
+++ front-end/components/userProfile/ProfileScrap.jsx
@@ -46,7 +46,7 @@ const ProfileScrap = () => {
                 <Link href={`/portfolio/${e._id}`}>
                   <div style={{ cursor: "pointer" }}>
                     <div>
-                      <img src="https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg" />
+                      <img src={e.thumbnail} />
                       <div id="gradation">
                         <div id="textbox">
                           <h3>{textLimitHandler(e.title, 15)}</h3>
