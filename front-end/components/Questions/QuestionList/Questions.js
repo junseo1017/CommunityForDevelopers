c59638dc7ef305c7f9d997420f78b47ff9e5bfd1
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-07-21 15:31:19 +0900

    검색 작업중

--- front-end/components/Questions/QuestionList/Questions.js
+++ front-end/components/Questions/QuestionList/Questions.js
@@ -13,26 +13,56 @@ const Questions = ({ questions, answers }) => {
   const [query, setQuery] = useState(""); // 유저가 입력한 검색어
   const [searchQueryString, setSearchQueryString] = useState(""); // 검색어 기반 url
 
-  console.log("questionsList", questionsList);
-  const [lastId, setLastId] = useState(questionsList[questionsList.length - 1]?._id);
-  console.log("lastId", lastId);
+  // const { ref, inView, entry } = useInView({
+  //   threshold: 0,
+  // });
 
-  useEffect(() => {
-    setSearchQueryString(`value=${query}&lastId=${lastId}`);
-    console.log(searchQueryString);
-  }, [query]);
+  // if (inView) {
+  //   console.log(inView);
+  // }
 
-  const getQnaData = useCallback(async () => {
-    // setIsLoading(true);
-    const response = await axios.get(`/api/search/qnas?${searchQueryString}`);
+  // useEffect(() => {
+  //   if (inView) {
+  //     setSearchQueryString(`value=${query}&lastId=${lastId}`);
+  //     getQnaData();
+  //   }
+  // }, [inView]);
 
-    setQuestionsList(response.data);
-    // setIsLoading(false);
-  });
+  // useEffect(() => {
+  //   const onScroll = throttle(() => {
+  //     if (query.length > 0) {
+  //       if (
+  //         window.scrollY + document.documentElement.clientHeight >
+  //         document.documentElement.scrollHeight - 300
+  //       ) {
+  //         if (query) {
+  //           await axios
+  //         }
+  //       }
+  //     }
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    getQnaData();
-  }, [getQnaData]);
+  // console.log("questionsList", questionsList);
+  // const [lastId, setLastId] = useState(questionsList[questionsList.length - 1]?._id);
+  // console.log("lastId", lastId);
+
+  // useEffect(() => {
+  //
+  //   console.log(searchQueryString);
+  // }, [query]);
+
+  // const getQnaData = useCallback(async () => {
+  //   // setIsLoading(true);
+  //   const response = await axios.get(`/api/search/qnas?${searchQueryString}`);
+
+  //   setQuestionsList(response.data);
+  //   // setIsLoading(false);
+  // });
+
+  // useEffect(() => {
+  //   getQnaData();
+  // }, [getQnaData]);
 
   // useEffect(() => {
   //   // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
@@ -56,13 +86,21 @@ const Questions = ({ questions, answers }) => {
       </div>
       <Divider plain />
       <div css={ColFlexBox}>
-        {questions.map((question) => {
+        {questions.map((question, idx) => {
           return (
             <div key={question._id}>
-              <QuestionItem
-                question={question}
-                answers={answers.filter((answer) => answer.parentQnaId === question._id)}
-              />
+              {idx === questions.length - 1 ? (
+                <QuestionItem
+                  // ref={ref}
+                  question={question}
+                  answers={answers.filter((answer) => answer.parentQnaId === question._id)}
+                />
+              ) : (
+                <QuestionItem
+                  question={question}
+                  answers={answers.filter((answer) => answer.parentQnaId === question._id)}
+                />
+              )}
               <Divider plain />
             </div>
           );
