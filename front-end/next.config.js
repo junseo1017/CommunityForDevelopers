498711f27e8d49a40cffce15ba02f36964a37354
RooT_Kim <bdd3452@gmail.com>
2022-07-22 22:37:06 +0900

    환경변수 설정[Oauth, next.config, app.ts]

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["localhost", "firebasestorage.googleapis.com"],
+    domains: [
+      "localhost",
+      "firebasestorage.googleapis.com",
+      "kdt-sw2-seoul-team06.elicecoding.com",
+    ],
   },
   eslint: { ignoreDuringBuilds: true },
 };
