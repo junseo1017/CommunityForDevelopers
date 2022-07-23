9f5440d24816531bb77f408149d39ae313c357a3
TJ <xowns1016@gmail.com>
2022-07-23 19:50:19 +0900

    joi validattion 추가

---
+++ back-end/src/db/schemas/joi-schemas/search-schema.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+const searchPortfolioJoi = Joi.object({
+  option: Joi.alternatives()
+    .try(Joi.array().items(Joi.string()), Joi.string())
+    .optional(),
+  value: Joi.string().optional(),
+  orderBy: Joi.string().optional(),
+  skill: Joi.alternatives()
+    .try(Joi.array().items(Joi.string()), Joi.string())
+    .optional(),
+  page: Joi.string().required(),
+});
+
+const searchQnaJoi = Joi.object({
+  value: Joi.string().optional(),
+  page: Joi.string().required(),
+});
+
+export { searchPortfolioJoi, searchQnaJoi };
